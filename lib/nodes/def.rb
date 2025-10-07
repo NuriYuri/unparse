0b6e4a29d3edbdebf0c171c59c84013ea8189aa2
Nuri Yuri <yuri@pokemonworkshop.com>
2025-10-07 20:46:38 +0200

    Experiment with method returning literals

--- lib/nodes/def.rb
+++ lib/nodes/def.rb
@@ -24,6 +24,25 @@ class MethodNode < OverridableNode
   end
 end
 
+module ValueMethod
+  SIMPLE_LITERALS = %i[int float sym str]
+
+  def is_value_method?
+    return @overwrite.is_value_method? if @overwrite
+    return false if @arguments&.children&.size > 0
+    return false unless @content
+    return false if @content.type == :return && @content.children.size != 1
+
+    node = @content.type == :return ? @content.children[0] : @content
+
+    return false if node.is_a?(BeginNode)
+    return true if SIMPLE_LITERALS.include?(node.type)
+    return false if node.type != :array
+    # Check array
+    return node.children.all? { |n| SIMPLE_LITERALS.include?(n.type) }
+  end
+end
+
 module WithMethodNode
   def n(type, children, location)
     return MethodNode.new(type, children, { location: }) if type == :def
@@ -33,3 +52,4 @@ module WithMethodNode
 end
 
 BuilderPrism.prepend(WithMethodNode)
+MethodNode.include(ValueMethod)
