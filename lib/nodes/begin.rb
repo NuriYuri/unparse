31ce0ffb19c8fb364b34d3fb7a967cfa82794723
Nuri Yuri <yuri@pokemonworkshop.com>
2025-10-08 17:35:42 +0200

    Fix issues introduced by begin optimizations

--- lib/nodes/begin.rb
+++ lib/nodes/begin.rb
@@ -1,4 +1,6 @@
 class BeginNode < OverridableNode
+  CLASS_LIKE_CHILDREN = %i[class sclass module]
+  OPERATORS = %i[/ * - + ** % ^ & | ~ ! > < >= <=]
   # @param type [Symbol]
   # @param children [Array<Parser::AST::Node | OverridableNode>]
   # @param props [Hash]
@@ -8,7 +10,14 @@ class BeginNode < OverridableNode
 
     children = children.reject { |c| c.is_a?(OverridableNode) && c.removed }
     return super(type, children, props) if children.size > 1
-    return map_child(children[0])
+
+    child = children[0]
+    return super unless child
+
+    return map_child(child) if child.is_a?(SendNode) && !OPERATORS.include?(child.method_name) && !child.method_name.end_with?('=')
+    return map_child(child) if CLASS_LIKE_CHILDREN.include?(child.type)
+
+    return super(type, children, props) 
   end
 
   # @param other [Parser::AST::Node | OverridableNode]
